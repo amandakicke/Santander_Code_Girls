015b64bbc9de8eef05064b494f578b2c3b057c17
amandakicke <amandakicke@gmail.com>
2022-08-03 10:47:48 -0300

    commit

---
+++ Introducao Spring Framework/meu-projeto-spring/src/main/java/br/com/meuPrimeiroProjeto/meuprojectspring/MeuProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuPrimeiroProjeto.meuprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjectSpringApplication.class, args);
+	}
+
+}
